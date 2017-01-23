34d8a2acee95447c54c66a1a31a7e0b856039cea
davidalves1 <alves.david@outlook.com>
2017-01-22 22:02:38 -0200

    Teste funcionando

--- bin/index.js
+++ bin/index.js
@@ -2,17 +2,68 @@
 
 'use strict';
 
+const https = require('https');
 const meow = require('meow');
 
 const cli = meow(`
-    How to use:
-      $ your-cli-command <params>
+    Modo de uso:
+      $ findcnpj <cnpj>
 
-    Example:
-      $ your-cli-command foo
+    Exemplo:
+      $ findcnpj 17184406000174
 
 `, {});
 
-var foo = process.argv.slice(2)[0];
+let cnpj = process.argv.slice(2)[0];
+cnpj = cnpj.replace(/\D/g, '');
 
-console.log(foo);
+if (cnpj.length !== 14) {
+	console.log('Ops, informe um CNPJ válido para consultar.');
+	return;
+}
+
+
+https
+	.get(`https://www.receitaws.com.br/v1/cnpj/${cnpj}`, 
+		response => {
+
+			let res = '';
+
+			response.on('data', data => {
+				res += data;
+			});
+
+			response.on('error', e => {
+				handleError(e);
+			});
+
+			response.on('end', () => {
+				res = JSON.parse(res);
+
+				if (res.status === 'OK')
+					handleSuccess(res);
+				else
+					handleError(res.message);
+
+		});
+	});
+
+function handleSuccess(data) {
+	console.log(`
+		Situação: ${data.situacao},
+		Atividade principal: ${data.atividade_principal[0].text},
+		Natureza jurídica: ${data.natureza_juridica},
+		Data de abertura: ${data.abertura},
+		Razão Social: ${data.nome},
+		Nome Fantasia: ${data.fantasia},
+		Logradouro: ${data.logradouro},
+		Número: ${data.numero},
+		Bairro: ${data.bairro},
+		Cidade: ${data.municipio},
+		Estado: ${data.uf}
+	`);
+}
+
+function handleError(err) {
+	console.log(err);
+}
